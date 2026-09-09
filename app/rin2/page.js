4be8c7e23ae10400d7d2e1f24a13dca9536f9036
ridhukhan <riyadkhan6598@gmail.com>
2026-09-09 23:14:14 +0600

    update

--- app/rin2/page.js
+++ app/rin2/page.js
@@ -195,7 +195,7 @@ export default function Rin() {
           {loading ? (
             <span className="text-gray-500">loading...</span>
           ) : (
-            `${calculateGrandOboshisto()} ৳`
+            `${calculateGrandOboshisto()} `
           )}
         </h1>
       </div>
@@ -234,7 +234,7 @@ export default function Rin() {
                         <div className="flex justify-between items-center flex-wrap gap-2">
                           <div className="w-full text-left">নাম:{member.name}</div>
                           {isAdmin && (
-                            <div className="flex gap-2 absolute right-6">
+                            <div className="flex gap-2  flex-col fixed">
                               <button
                                 onClick={() => handleEdit(member)}
                                 className="bg-blue-500 text-white font-bold text-xs px-2 py-1 rounded border border-black"
