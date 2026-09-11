7e055373cb87651cfcde97b590d400faf9c8222d
ridhukhan <riyadkhan6598@gmail.com>
2026-09-12 00:30:13 +0600

    update

--- app/rin3/page.js
+++ app/rin3/page.js
@@ -354,7 +354,7 @@ export default function Rin() {
                                     {isAdmin && (
                                       <div
                                         {...provided.dragHandleProps}
-                                        className="cursor-grab active:cursor-grabbing p-1 hover:bg-yellow-600/30 rounded touch-none flex items-center"
+                                        className="cursor-grab left-2 active:cursor-grabbing p-1 hover:bg-yellow-600/30 rounded touch-none flex items-center"
                                         title="ড্র্যাগ করে সরান"
                                       >
                                         <svg
